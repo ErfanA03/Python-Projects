82a762a36fcd8e23efefb5aac562f11ab957e033
Erfan Arsin <erfanbro46@gmail.com>
2023-09-19 14:40:10 -0600

    Cleaned up encapsulation assignment

--- encapsulation_assignment.py
+++ encapsulation_assignment.py
@@ -5,9 +5,8 @@ class MyClass1:
     def __private_method(self):
         print("This is a private method")
 
-obj = MyClass1()
-print(obj.__private_var)  # Raises an error, not accessible outside the class
-obj.__private_method()    # Raises an error, not accessible outside the class
+    def get_private_var(self):
+        return self.__private_var  # Public method to access private attribute
 
 
 class MyClass2:
@@ -17,10 +16,21 @@ class MyClass2:
     def _protected_method(self):
         print("This is a protected method")
 
-class MyDerivedClass(MyClass):
+    def get_protected_var(self):
+        return self._protected_var  # Public method to access protected attribute
+
+
+class MyDerivedClass(MyClass1):
     def access_protected(self):
-        print(self._protected_var)  # Accessing protected attribute from a subclass
-        self._protected_method()     # Accessing protected method from a subclass
+        # Accessing protected attribute and methods from MyClass2
+        base_class = MyClass2()
+        print("Protected Var from Base Class: {}".format(base_class.get_protected_var()))
+        base_class._protected_method()  # Accessing protected method
+
+        # Accessing private attribute and method from MyClass1
+        print("Private Var from Superclass: {}".format(self.get_private_var()))
+        self._MyClass1__private_method()  # Accessing private method
+
 
 obj = MyDerivedClass()
 obj.access_protected()
